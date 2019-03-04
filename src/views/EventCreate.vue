3468be50ca9904daed5507a8dff11699ffe76949
aspiringguru <bmatthewtaylor@gmail.com>
2019-03-04 14:24:11 +1000

    first commit

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+    <p>this event was created by</p>
+  </div>
+</template>
+
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
